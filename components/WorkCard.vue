73c1414c7a6222ebe2450baced8be817847884d0
Zokiasu <pierrick.tly@gmail.com>
2022-02-27 18:08:43 +0100

    fix: fix workdcard

--- components/WorkCard.vue
+++ components/WorkCard.vue
@@ -5,11 +5,11 @@
       :src="workImage"
       class="bg-gray-500 object-cover rounded-lg aspect-video w-full group-hover:blur-sm transition-all duration-300"
     />
-    <div class="bg-black/30 absolute top-0 w-full group-hover:h-full transition-all transform ease-in-out duration-500 flex flex-col justify-between py-2">
+    <div class="bg-black/30 absolute top-0 w-full group-hover:h-full transition transform ease-in-out duration-1000 flex flex-col justify-between py-2">
       <h2 class="px-2 font-semibold text-xl">{{workName}}</h2>
-      <p class="px-3 hidden group-hover:block transition-all transform ease-in-out duration-500">{{workDescription}}</p>
-      <div class="px-3 w-full group-hover:flex flex-row flex-wrap gap-1 text-sm hidden transition-all transform ease-in-out duration-500">
-        <p v-for="tech in workTechnology" :key="tech" class="px-3 py-1 border border-white">{{tech}}</p>
+      <p class="px-5 hidden group-hover:block transition-all transform ease-in-out duration-500">{{workDescription}}</p>
+      <div class="px-5 pb-1 w-full group-hover:flex flex-row flex-wrap gap-1 text-sm hidden transition-all transform ease-in-out duration-500">
+        <p v-for="tech in workTechnology" :key="tech" class="px-3 py-1 border rounded border-white bg-black/30">{{tech}}</p>
       </div>
     </div>
   </a>
