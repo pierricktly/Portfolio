51b6d603e91a80abc387f6fdef10e18769c31c6e
Zokiasu <pierrick.tly@gmail.com>
2022-03-09 09:08:41 +0100

    fix: about in index

---
+++ components/Icon/IconLongArrowDown.vue
@@ -0,0 +1,17 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+		<path
+			d="m5.208 13.542 7.292 7.291 7.292-7.291M12.5 20.833V4.167v16.666Z"
+			stroke="currentColor"
+			stroke-width="2.083"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</svg>
+</template>
+
+<script>
+	export default {
+		name: 'IconLongArrowDown'
+	};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,29 @@
 <template>
-  <div>
+  <div  data-scroll-container>
     <header class="absolute w-full top-0 bg-primary z-50">
       <navigation/>
     </header>
     <main>
       <Nuxt />
     </main>
+    <footer>
+      <div id="copyright" data-scroll-section class="w-full container mx-auto text-base p-5 text-white">
+        <p class="text-center">©2022 Zokiasu - Portfolio</p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
   export default {
     transition: 'page',  
+
+    mounted() {
+      this.scroll = new this.locomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+      });
+    },
   }
 </script>
 
