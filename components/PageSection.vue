90bb062f80aea20af21641e32d16fe729e779868
Zokiasu <pierrick.tly@gmail.com>
2022-04-25 01:40:23 +0200

    add section component

---
+++ components/PageSection.vue
@@ -0,0 +1,19 @@
+<template>
+	<section class="mx-auto space-y-8 p-8">
+		<h2 class="text-center text-secondary font-bold text-4xl">
+			<slot name="title"/>
+		</h2>
+		<p class="text-center text-xl">
+			<slot name="subtitle"/>
+		</p>
+		<div>
+			<slot name="default"/>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'StackSection'
+}
+</script>
