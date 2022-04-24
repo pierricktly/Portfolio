d4bb48ee6be92fd48ee1bae2e0899c6303fe14b7
Zokiasu <pierrick.tly@gmail.com>
2022-04-25 01:40:10 +0200

    add stack component

---
+++ components/CategoryStack.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="w-full mx-auto space-y-5">
+		<p class="text-center font-semibold text-3xl">{{ categoryName }}</p>
+		<ul class="flex flex-wrap justify-center gap-x-20 gap-y-5">
+			<li v-for="stack in stackList" :key="stack.name">
+				<stack :icon-name="stack.icon" :text="stack.name" />
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "CategoryStack",
+		
+		props: {
+			categoryName: {
+				type: String,
+				required: true
+			},
+			stackList: {
+				type: Array,
+				required: true
+			}
+		}
+	}
+</script>
---
+++ components/Stack.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="space-y-1">
+		<component :is="`icon-${iconName}`" class="w-16 h-16 mx-auto"></component>
+		<p class="uppercase font-medium text-center">{{text}}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Stack",
+		
+		props: {
+			iconName: {
+				type: String,
+				required: true
+			},
+			text: {
+				type: String,
+				required: true
+			}
+		}
+	}
+</script>
