a277c3b582a0ff5a3c46f9649b49ee913aba3b0a
Zokiasu <pierrick.tly@gmail.com>
2022-06-12 17:04:53 +0200

    fix link top

--- pages/index.vue
+++ pages/index.vue
@@ -52,17 +52,17 @@
 					</div>
 					<ul class="mx-auto flex items-center space-x-8">
 						<li>
-							<a href="">
+							<a href="https://www.linkedin.com/in/pierrick-taily/" target="_blank">
 								<icon-linkedin class="w-10 h-10 transition-all ease-in-out duration-300 hover:text-secondary"/>
 							</a>
 						</li>
 						<li>
-							<a href="">
+							<a href="https://github.com/Zokiasu" target="_blank">
 								<icon-github class="w-10 h-10 transition-all ease-in-out duration-300 hover:text-secondary"/>
 							</a>
 						</li>
 						<li>
-							<a href="">
+							<a href="https://www.malt.fr/profile/zokiasu" target="_blank">
 								<icon-malt class="w-10 h-10 transition-all ease-in-out duration-300 hover:text-secondary"/>
 							</a>
 						</li>
