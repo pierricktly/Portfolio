bb4216e9af27e094d5144005f85c6db648f8e158
Pierrick Taïly <pierrick.tly@gmail.com>
2022-12-14 06:17:51 +0100

    test

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,61 +1,10 @@
 <template>
-  <div class="text-white">
-    <div class="container mx-auto flex justify-between py-5 px-5 xl:px-0">
-      <div>
-        <a href="/">
-          <h1 class="font-bold text-2xl select-none">Zokiasu.</h1>
-        </a>
-      </div>
-      <div>
-        <button @click="navMenu = !navMenu" type="button" class="lg:hidden">
-          <icon-burger class="w-5 h-5"/>
-        </button>
-        <ul class="hidden lg:flex space-x-20 text-xl">
-          <li>
-            <nuxt-link to="#service" class="hover-underline-animation">Services</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="#work" class="hover-underline-animation">Portfolio</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="#contact" class="hover-underline-animation">Contact</nuxt-link>
-          </li>
-        </ul>
-      </div>
+  <div>
+    <div>
+      
     </div>
-
-    <!-- Mobile menu, show/hide based on menu state. -->
-    <div
-      v-if="navMenu"
-      class="animate__animated animate__fadeInDown animate__faster top-16 md:top-20 origin-top absolute bg-primary w-full lg:hidden"
-      id="mobile-menu"
-    >
-      <div class="px-2 pt-2 pb-3 space-y-1" @click="navMenu = false">
-        <nuxt-link
-          :to="`/`"
-          class="block px-3 py-2 rounded-md font-medium hover:bg-white/10"
-        >
-          Home
-        </nuxt-link>
-        <nuxt-link
-          to="#service"
-          class="block px-3 py-2 rounded-md font-medium hover:bg-white/10"
-        >
-          Services
-        </nuxt-link>
-        <nuxt-link
-          to="#work"
-          class="block px-3 py-2 rounded-md font-medium hover:bg-white/10"
-        >
-          Portfolio
-        </nuxt-link>
-        <nuxt-link 
-          to="#contact"
-          class="block px-3 py-2 rounded-md font-medium hover:bg-white/10"
-        >
-          Contact
-        </nuxt-link>
-      </div>
+    <div>
+      
     </div>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,43 @@
 <template>
-  <div class="scroll-smooth">
-    <header class="sticky top-0 md:pt-5 z-10 transition-all transform duration-700 ease-out">
+  <div>
+    <header>
       <navigation />
     </header>
     <main>
       <Nuxt />
     </main>
-    <footer>
+    <!-- <footer>
       <footer-section />
-    </footer>
+    </footer> -->
   </div>
 </template>
 
 <script>
   export default {
     transition: 'page',
-    data() {
-      return {
-        scroll: 0,
-      };
-    },
-    mounted() {
-      window.addEventListener('scroll', this.handleScroll);
-    },
-    destroyed() {
-      window.removeEventListener('scroll', this.handleScroll);
-    },
-    methods: {
-      handleScroll() {
-        this.scroll = window.scrollY;
-        if (this.scroll > window.innerHeight * 0.05) {
-          this.$el.querySelector('header').classList.add('bg-primary');
-          this.$el.querySelector('header').classList.remove('md:pt-5');
-        } else {
-          this.$el.querySelector('header').classList.remove('bg-primary');
-          this.$el.querySelector('header').classList.add('md:pt-5');
-        }
-      },
-    },
+    // data() {
+    //   return {
+    //     scroll: 0,
+    //   };
+    // },
+    // mounted() {
+    //   window.addEventListener('scroll', this.handleScroll);
+    // },
+    // destroyed() {
+    //   window.removeEventListener('scroll', this.handleScroll);
+    // },
+    // methods: {
+    //   handleScroll() {
+    //     this.scroll = window.scrollY;
+    //     if (this.scroll > window.innerHeight * 0.05) {
+    //       this.$el.querySelector('header').classList.add('bg-primary');
+    //       this.$el.querySelector('header').classList.remove('md:pt-5');
+    //     } else {
+    //       this.$el.querySelector('header').classList.remove('bg-primary');
+    //       this.$el.querySelector('header').classList.add('md:pt-5');
+    //     }
+    //   },
+    // },
   };
 </script>
 
