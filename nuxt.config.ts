b517916a777ecb53f1c50e3fc2ce244639ba9ba1
Pierrick Taïly <pierrick.tly@gmail.com>
2023-08-06 20:23:10 +0200

    update seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,38 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      title: 'Pierrick TAÏLY - Portfolio - Développeur Frontend Vue.js Nuxt.js',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { hid: 'description', name: 'description', content: 'Retrouver mes derniers projets personnel' },
+        // ogDescription, ogTitle, ogImage, ogUrl, ogSiteName
+        { hid: 'og:description', name: 'og:description', content: 'Retrouver mes derniers projets personnel' },
+        { hid: 'og:title', name: 'og:title', content: 'Pierrick TAÏLY - Portfolio - Développeur Frontend Vue.js Nuxt.js' },
+        { hid: 'og:image', name: 'og:image', content: 'https://pierricktly.xyz/picture.jpg' },
+        { hid: 'og:url', name: 'og:url', content: 'https://pierricktly.xyz' },
+        { hid: 'og:site_name', name: 'og:site_name', content: 'Pierrick TAÏLY - Portfolio - Développeur Frontend Vue.js Nuxt.js' },
+        // twitterCard, twitterSite, twitterCreator
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@pierricktly' },
+        { hid: 'twitter:creator', name: 'twitter:creator', content: '@pierricktly' },
+        { hid: 'twitter:image', name: 'twitter:image', content: 'https://pierricktly.xyz/picture.jpg' },
+        { hid: 'twitter:url', name: 'twitter:url', content: 'https://pierricktly.xyz' },
+        { hid: 'twitter:title', name: 'twitter:title', content: 'Pierrick TAÏLY - Portfolio - Développeur Frontend Vue.js Nuxt.js' },
+        { hid: 'twitter:description', name: 'twitter:description', content: 'Retrouver mes derniers projets personnel' },
+
+
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/image',
